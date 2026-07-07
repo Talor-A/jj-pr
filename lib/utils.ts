d8e167284d6be459bad7322261f6a5c2b960c78a
Talor <public-github@taloranderson.com>
2026-07-07 12:33:26 -0700

    share parseJsonLines in lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,13 @@
+import type { z } from "zod";
+
 export function lines(value: string): string[] {
   return value.split(/\r?\n/).filter((line) => line.length > 0);
 }
 
 export function unique<T>(values: T[]): T[] {
   return [...new Set(values)];
+}
+
+export function parseJsonLines<T>(schema: z.ZodType<T>, value: string): T[] {
+  return lines(value).map((line) => schema.parse(JSON.parse(line)));
 }
